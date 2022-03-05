cf0f3e9b7e051bee3091f51d55df492322d117fb
V0idMatr1x <chrispaige.dev@gmail.com>
2022-03-04 21:55:22 -0600

    addded proper unicode symbol for each token

--- src/main.py
+++ src/main.py
@@ -44,63 +44,45 @@ def main(greeting):
         cg.get_price(ids="bitcoin-cash", vs_currencies="usd")["bitcoin-cash"]["usd"],
         cg.get_price(ids="litecoin", vs_currencies="usd")["litecoin"]["usd"],
         cg.get_price(ids="cardano", vs_currencies="usd")["cardano"]["usd"],
-        cg.get_price(ids="stellar", vs_currencies="usd")["stellar"]["usd"],
         cg.get_price(ids="monero", vs_currencies="usd")["monero"]["usd"],
         cg.get_price(ids="dash", vs_currencies="usd")["dash"]["usd"],
-        cg.get_price(ids="iota", vs_currencies="usd")["iota"]["usd"],
         cg.get_price(ids="ethereum-classic", vs_currencies="usd")["ethereum-classic"][
             "usd"
         ],
-        cg.get_price(ids="nem", vs_currencies="usd")["nem"]["usd"],
-        cg.get_price(ids="tron", vs_currencies="usd")["tron"]["usd"],
         cg.get_price(ids="tezos", vs_currencies="usd")["tezos"]["usd"],
     ]
 
     # Title
     stdout(n.bold_text(n.green_text(">> Crypto Prices 📈" + "\n")))
     
-    btc = n.red_text("\n 💸 Bitcoin Val: $" + str(crypto[0]) + " USD")
+    btc = n.red_text("\n ₿ Bitcoin Val: $" + str(crypto[0]) + " USD")
     stdout(btc)
     stdout(n.cyan_text("________________________________________________/"))
-    eth = n.red_text("\n 💸 Ethereum Val: $" + str(crypto[1]) + " USD")
+    eth = n.red_text("\n ⟠ Ethereum Val: $" + str(crypto[1]) + " USD")
     stdout(eth)
     stdout(n.cyan_text("________________________________________________/"))
-    xrp = n.red_text("\n 💸 Ripple Val: $" + str(crypto[2]) + " USD")
+    xrp = n.red_text("\n ✕ Ripple Val: $" + str(crypto[2]) + " USD")
     stdout(xrp)
     stdout(n.cyan_text("________________________________________________/"))
-    bch = n.red_text("\n 💸 Bitcoin Cash Val: $" + str(crypto[3]) + " USD")
+    bch = n.red_text("\n Ƀ Bitcoin Cash Val: $" + str(crypto[3]) + " USD")
     stdout(bch)
     stdout(n.cyan_text("________________________________________________/"))
-    ltc = n.red_text("\n 💸 Litecoin Val: $" + str(crypto[4]) + " USD")
+    ltc = n.red_text("\n Ł Litecoin Val: $" + str(crypto[4]) + " USD")
     stdout(ltc)
     stdout(n.cyan_text("________________________________________________/"))
-    ada = n.red_text("\n 💸 Cardano Val: $" + str(crypto[5]) + " USD")
+    ada = n.red_text("\n ₳ Cardano Val: $" + str(crypto[5]) + " USD")
     stdout(ada)
     stdout(n.cyan_text("________________________________________________/"))
-    xlm = n.red_text("\n 💸 Stellar Val: $" + str(crypto[6]) + " USD")
-    stdout(xlm)
-    stdout(n.cyan_text("________________________________________________/"))
-    xmr = n.red_text("\n 💸 Monero Val: $" + str(crypto[7]) + " USD")
+    xmr = n.red_text("\n ɱ Monero Val: $" + str(crypto[6]) + " USD")
     stdout(xmr)
     stdout(n.cyan_text("________________________________________________/"))
-    dash = n.red_text("\n 💸 Dash Val: $" + str(crypto[8]) + " USD")
+    dash = n.red_text("\n Đ Dash Val: $" + str(crypto[7]) + " USD")
     stdout(dash)
     stdout(n.cyan_text("________________________________________________/"))
-    iota = n.red_text("\n 💸 Iota Val: $" + str(crypto[9]) + " USD")
-    stdout(iota)
-    stdout(n.cyan_text("________________________________________________/"))
-    ecl = n.red_text("\n 💸 Ethereum Classic Val: $" + str(crypto[10]) + " USD")
+    ecl = n.red_text("\n ξ Ethereum Classic Val: $" + str(crypto[8]) + " USD")
     stdout(ecl)
     stdout(n.cyan_text("________________________________________________/"))
-    nem = n.red_text("\n 💸 NEM Val: $" + str(crypto[11]) + " USD")
-    stdout(nem)
-    stdout(n.cyan_text("________________________________________________/"))
-    trx = n.red_text("\n 💸 Tron Val: $" + str(crypto[12]) + " USD")
-    stdout(trx)
-    stdout(n.cyan_text("________________________________________________/"))
-    tezos = n.red_text("\n 💸 Tezos Val: $" + str(crypto[13]) + " USD")
+    tezos = n.red_text("\n ꜩ Tezos Val: $" + str(crypto[9]) + " USD")
     stdout(tezos)
-    stdout(n.cyan_text("________________________________________________/"))
-
 
 main(n.purple_text(n.bold_text("🤖 Welcome, V0idMatr1x!\n")))
