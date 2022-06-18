08462d13501b12357d1c6caf78269f0918bdd32a
V0idMatr1x <88049272+V0idMatr1x@users.noreply.github.com>
2022-06-18 23:46:19 +0000

    Update ext.nim

--- src/ext.nim
+++ src/ext.nim
@@ -6,27 +6,21 @@ import run_exe
 proc clear(x: string): string {.exportpy.} =
     run exe x
 
-
 # Text Styles
 proc red_text(text: string): string {.exportpy.} =
   return "[red]" & text
 
-
 proc green_text(text: string): string {.exportpy.} =
   return "[green]" & text
-
 
 proc purple_text(text: string): string {.exportpy.} =
   return "[purple]" & text
 
-
 proc cyan_text(text: string): string {.exportpy.} =
   return "[cyan]" & text
 
-
 proc bold_text(text: string): string {.exportpy.} =
   return "[bold]" & text
-
 
 proc underline_text(text: string): string {.exportpy.} =
   return "[underline]" & text
